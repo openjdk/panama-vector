e901c4d96fc61ba73098ee796efa144c89ea409b
Xiaohong Gong <xgong@openjdk.org>
2021-02-20 06:30:44 +0000

    8261394: [vector] Crash with "assert(Matcher::vector_size_supported(elem_bt, length)) failed: length in range"
    Reviewed-by: njian, jiefu, sviswanathan

--- src/hotspot/share/opto/vectorIntrinsics.cpp
+++ src/hotspot/share/opto/vectorIntrinsics.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -250,6 +250,13 @@ bool LibraryCallKit::inline_vector_nary_operation(int n) {
     if (!Matcher::supports_vector_calling_convention()) {
       if (C->print_intrinsics()) {
         tty->print_cr("  ** no vector calling conventions supported");
+      }
+      return false;
+    }
+    if (!Matcher::vector_size_supported(elem_bt, num_elem)) {
+      if (C->print_intrinsics()) {
+        tty->print_cr("  ** vector size (vlen=%d, etype=%s) is not supported",
+                      num_elem, type2name(elem_bt));
       }
       return false;
     }
