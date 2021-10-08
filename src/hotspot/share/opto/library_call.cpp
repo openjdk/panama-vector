63a9fe9d6d1982d3fe759c856ab417c3ff33bc55
Joshua Zhu <jzhu@openjdk.org>
2021-10-08 04:45:29 +0000

    8274839: Intrinsic support for compress & expand VectorAPI
    Reviewed-by: sviswanathan

--- src/hotspot/share/opto/library_call.cpp
+++ src/hotspot/share/opto/library_call.cpp
@@ -680,6 +680,8 @@ bool LibraryCallKit::try_to_inline(int predicate) {
     return inline_vector_insert();
   case vmIntrinsics::_VectorExtract:
     return inline_vector_extract();
+  case vmIntrinsics::_VectorSelectiveOp:
+    return inline_vector_compress_expand();
 
   case vmIntrinsics::_getObjectSize:
     return inline_getObjectSize();
