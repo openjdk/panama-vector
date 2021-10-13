c4d51d6f921b90c3e59949bb67464e0b5949de25
Joshua Zhu <yueshi.zwj@alibaba-inc.com>
2021-10-13 19:39:47 +0800

    update

--- test/micro/org/openjdk/bench/jdk/incubator/vector/bigdata/SelectiveStore.java
+++ test/micro/org/openjdk/bench/jdk/incubator/vector/bigdata/SelectiveStore.java
@@ -26,7 +26,6 @@ package org.openjdk.bench.jdk.incubator.vector.bigdata;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.ThreadLocalRandom;
 
 import jdk.incubator.vector.IntVector;
 import jdk.incubator.vector.VectorSpecies;
@@ -38,13 +37,16 @@ import org.openjdk.jmh.annotations.*;
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
-@Warmup(iterations = 5, time = 1)
+@Warmup(iterations = 10, time = 1)
 @Measurement(iterations = 10, time = 1)
-@Fork(value = 1, jvmArgsPrepend = {"--add-modules=jdk.incubator.vector", "--add-exports", "java.base/jdk.internal.vm.vector=ALL-UNNAMED", "-ea"})
+@Fork(value = 1, jvmArgsPrepend = {"--add-modules=jdk.incubator.vector"})
 public class SelectiveStore {
 
-  private static int ARRAY_LENGTH = 1024;
+  @Param("1024")
+  int ARRAY_LENGTH;
 
+  private static final VectorSpecies<Integer> INT_64_SPECIES =
+    IntVector.SPECIES_64;
   private static final VectorSpecies<Integer> INT_128_SPECIES =
     IntVector.SPECIES_128;
   private static final VectorSpecies<Integer> INT_256_SPECIES =
@@ -54,136 +56,83 @@ public class SelectiveStore {
   private static final VectorSpecies<Integer> INT_PREFERRED_SPECIES =
     IntVector.SPECIES_PREFERRED;
 
-  @State(Scope.Thread)
-  public static class ThreadLocalData {
+  private int conflict_cnt;
+  private int[] index;
+  private int[] input1;
+  private int[] input2;
+  private int[] conflict_array;
 
-    private int conflict_scalar_cnt = 0;
-    private int conflict_vector_cnt = 0;
-    private int[] index = new int[ARRAY_LENGTH];
-    private int[] conflict_scalar = new int[ARRAY_LENGTH];
-    private int[] conflict_vector = new int[ARRAY_LENGTH];
-    private int[] input1 = new int[ARRAY_LENGTH];
-    private int[] input2 = new int[ARRAY_LENGTH];
-    private Random rand = new Random();
+  @Setup
+  public void init() {
+    index = new int[ARRAY_LENGTH];
+    input1 = new int[ARRAY_LENGTH];
+    input2 = new int[ARRAY_LENGTH];
+    conflict_array = new int[ARRAY_LENGTH];
 
-    @Setup(Level.Trial)
-    public void doSetup() {
-      int conflict_id_each_5_elements = ThreadLocalRandom.current().nextInt(5);
+    Random rand = new Random();
+    int conflict_id_each_5_elements = rand.nextInt(5);
 
-      for (int i = 0; i < ARRAY_LENGTH; i++) {
-        index[i] = ThreadLocalRandom.current().nextInt(ARRAY_LENGTH);
-        input1[i] = ThreadLocalRandom.current().nextInt();
-        // Generate 20% conflict data
-        int remainder = i % 5;
-        if (remainder == conflict_id_each_5_elements) {
-          input2[i] = ThreadLocalRandom.current().nextInt();
-        } else {
-          input2[i] = input1[i];
-        }
-
-        conflict_scalar[i] = 0;
-        conflict_vector[i] = 0;
-      }
-    }
-  }
-
-  @Benchmark
-  @Threads(8)
-  public void selectiveStore_scalar(ThreadLocalData tld) {
-    tld.conflict_scalar_cnt = 0;
     for (int i = 0; i < ARRAY_LENGTH; i++) {
-      if (tld.input1[i] != tld.input2[i]) {
-        tld.conflict_scalar[tld.conflict_scalar_cnt++] = tld.index[i];
-      }
-    }
-  }
-
-  @Benchmark
-  @Threads(8)
-  public void selectiveStore_vector_128(ThreadLocalData tld) {
-    tld.conflict_vector_cnt = 0;
-    for (int i = 0; i < ARRAY_LENGTH; i += INT_128_SPECIES.length()) {
-      IntVector av = IntVector.fromArray(INT_128_SPECIES, tld.input1, i);
-      IntVector bv = IntVector.fromArray(INT_128_SPECIES, tld.input2, i);
-      IntVector cv = IntVector.fromArray(INT_128_SPECIES, tld.index, i);
-      VectorMask<Integer> mask = av.compare(VectorOperators.NE, bv);
-      IntVector dv = cv.compress(mask);
-      dv.intoArray(tld.conflict_vector, tld.conflict_vector_cnt, mask.prefixMask());
-      tld.conflict_vector_cnt += mask.trueCount();
-    }
-  }
-
-  @Benchmark
-  @Threads(8)
-  public void selectiveStore_vector_256(ThreadLocalData tld) {
-    tld.conflict_vector_cnt = 0;
-    for (int i = 0; i < ARRAY_LENGTH; i += INT_256_SPECIES.length()) {
-      IntVector av = IntVector.fromArray(INT_256_SPECIES, tld.input1, i);
-      IntVector bv = IntVector.fromArray(INT_256_SPECIES, tld.input2, i);
-      IntVector cv = IntVector.fromArray(INT_256_SPECIES, tld.index, i);
-      VectorMask<Integer> mask = av.compare(VectorOperators.NE, bv);
-      IntVector dv = cv.compress(mask);
-      dv.intoArray(tld.conflict_vector, tld.conflict_vector_cnt, mask.prefixMask());
-      tld.conflict_vector_cnt += mask.trueCount();
-    }
-  }
-
-  @Benchmark
-  @Threads(8)
-  public void selectiveStore_vector_512(ThreadLocalData tld) {
-    tld.conflict_vector_cnt = 0;
-    for (int i = 0; i < ARRAY_LENGTH; i += INT_512_SPECIES.length()) {
-      IntVector av = IntVector.fromArray(INT_512_SPECIES, tld.input1, i);
-      IntVector bv = IntVector.fromArray(INT_512_SPECIES, tld.input2, i);
-      IntVector cv = IntVector.fromArray(INT_512_SPECIES, tld.index, i);
-      VectorMask<Integer> mask = av.compare(VectorOperators.NE, bv);
-      IntVector dv = cv.compress(mask);
-      dv.intoArray(tld.conflict_vector, tld.conflict_vector_cnt, mask.prefixMask());
-      tld.conflict_vector_cnt += mask.trueCount();
-    }
-  }
-
-  @Benchmark
-  @Threads(8)
-  public void selectiveStore_vector_preferred(ThreadLocalData tld) {
-    tld.conflict_vector_cnt = 0;
-    for (int i = 0; i < ARRAY_LENGTH; i += INT_PREFERRED_SPECIES.length()) {
-      IntVector av = IntVector.fromArray(INT_PREFERRED_SPECIES, tld.input1, i);
-      IntVector bv = IntVector.fromArray(INT_PREFERRED_SPECIES, tld.input2, i);
-      IntVector cv = IntVector.fromArray(INT_PREFERRED_SPECIES, tld.index, i);
-      VectorMask<Integer> mask = av.compare(VectorOperators.NE, bv);
-      IntVector dv = cv.compress(mask);
-      dv.intoArray(tld.conflict_vector, tld.conflict_vector_cnt, mask.prefixMask());
-      tld.conflict_vector_cnt += mask.trueCount();
-    }
-  }
-
-  @Benchmark
-  @Threads(8)
-  public void selectiveStore_verify(ThreadLocalData tld) {
-    // Initialization
-    int conflict_id_each_5_elements = ThreadLocalRandom.current().nextInt(5);
-    for (int i = 0; i < ARRAY_LENGTH; i++) {
-      tld.index[i] = ThreadLocalRandom.current().nextInt(ARRAY_LENGTH);
-      tld.input1[i] = ThreadLocalRandom.current().nextInt();
+      index[i] = rand.nextInt(ARRAY_LENGTH);
+      input1[i] = rand.nextInt();
       // Generate 20% conflict data
       int remainder = i % 5;
       if (remainder == conflict_id_each_5_elements) {
-        tld.input2[i] = ThreadLocalRandom.current().nextInt();
+        input2[i] = rand.nextInt();
       } else {
-        tld.input2[i] = tld.input1[i];
+        input2[i] = input1[i];
       }
-      tld.conflict_scalar[i] = 0;
-      tld.conflict_vector[i] = 0;
+      conflict_array[i] = 0;
     }
-    // Test
-    selectiveStore_scalar(tld);
-    selectiveStore_vector_preferred(tld);
-    // Verify
+  }
+
+  private void selectiveStore(VectorSpecies<Integer> species) {
+    conflict_cnt = 0;
+    for (int i = 0; i < ARRAY_LENGTH; i += species.length()) {
+      IntVector av = IntVector.fromArray(species, input1, i);
+      IntVector bv = IntVector.fromArray(species, input2, i);
+      IntVector cv = IntVector.fromArray(species, index, i);
+      VectorMask<Integer> mask = av.compare(VectorOperators.NE, bv);
+      IntVector dv = cv.compress(mask);
+      dv.intoArray(conflict_array, conflict_cnt, mask.compress());
+      conflict_cnt += mask.trueCount();
+    }
+  }
+
+  @Benchmark
+  public void selectiveStore_scalar() {
+    conflict_cnt = 0;
     for (int i = 0; i < ARRAY_LENGTH; i++) {
-      assert(tld.conflict_scalar[i] == tld.conflict_vector[i]);
+      if (input1[i] != input2[i]) {
+        conflict_array[conflict_cnt] = index[i];
+        conflict_cnt++;
+      }
     }
-    assert(tld.conflict_scalar_cnt == tld.conflict_vector_cnt);
+  }
+
+  @Benchmark
+  public void selectiveStore_vector_64() {
+    selectiveStore(INT_64_SPECIES);
+  }
+
+  @Benchmark
+  public void selectiveStore_vector_128() {
+    selectiveStore(INT_128_SPECIES);
+  }
+
+  @Benchmark
+  public void selectiveStore_vector_256() {
+    selectiveStore(INT_256_SPECIES);
+  }
+
+  @Benchmark
+  public void selectiveStore_vector_512() {
+    selectiveStore(INT_512_SPECIES);
+  }
+
+  @Benchmark
+  public void selectiveStore_vector_preferred() {
+    selectiveStore(INT_PREFERRED_SPECIES);
   }
 }
 
